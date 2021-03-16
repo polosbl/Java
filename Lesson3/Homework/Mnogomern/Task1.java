d630613c743cb63c7402306121f29c6189b63731
Alexander Gordeichik <a.gordeichik@invento.by>
2021-03-16 22:21:17 +0300

    Lesson4

---
+++ Lesson3/Homework/Mnogomern/Task1.java
@@ -0,0 +1,17 @@
+package Lesson3.Homework.Mnogomern;
+
+import java.util.Arrays;
+
+public class Task1 {
+        public static String[] strings = new String[]{"I", "love", "Java"};
+        public static int[] ints = new int[]{1, 2, 3, 4, 5, 6, 7, 8};
+
+        public static void main(String[] args) {
+            String a = Arrays.toString(strings);
+            System.out.println(a);
+            String b = Arrays.toString(ints);
+            System.out.println(b);
+
+            //напишите тут ваш код
+        }
+}
